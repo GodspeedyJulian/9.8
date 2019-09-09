230ebef9062a88be2c7d9cc9bdad092213f05e41
Julian Ye <50823612+GodspeedyJulian@users.noreply.github.com>
2019-09-09 15:49:51 +0800

    Add files via upload

---
+++ 练习卷2题编程.py
@@ -0,0 +1,8 @@
+startpos=int(input("the array start position"))
+NumToChange= int(input("the number of elemnets to change"))
+n=0
+while n !=NumToChange-1:
+    NewChar=input("input a new substitute character")
+    Lookup [Startpos+n]=NewChar,
+    n+=1
+print(NumToChange, "elements have been changed")
